4080cfb6d00737c643f49541d1dcd4e8567f5455
okunlade Jolade <jokunlade@gmail.com>
2022-04-16 12:25:55 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("Hello world");
+let name = 'Jolade';
+console.log(name);
+let firstName = 'Jolade';
+let lastName = 'Okunlade';
+
+let intrestRate = 0.3;
+intrestRate = 1;
+console.log(intrestRate);
+let age = 30;
+let isApproved = true;
+let selectedColor = null;
+let person = {
+name: 'okunlade jolade',
+address: 'fagba',
+phoneNo: 08126678773,
+office: 'software engineer',
+age: 26
+};
+console.log(person);
+let selectedColors = ['pink', 'white', 'yellow', 'red'];
+selectedColors[4] = 'purple'; 
+console.log(selectedColors);
+function greet (name) {
+    console.log('hello ' + name);
+}
+greet('Jola');
+greet('Derin');
+greet('sandy');
+greet('bola');
+
+function square(number) {
+    return number * number;
+}
+
+let number = square(2);
+console.log(number);
+
+console.log(square(4));
